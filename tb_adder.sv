019fcd8e54cfdee335fafb5a410f745905b1ab00
DAEYEONG <nightmare2585@gmail.com>
2026-03-13 15:21:53 +0900

    Add file for the test tb_adder.sv

---
+++ tb_adder.sv
@@ -0,0 +1,24 @@
+module tb;
+
+logic [7:0] a;
+logic [7:0] b;
+logic [7:0] y;
+
+adder dut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a = 10;
+    b = 20;
+
+    #1;
+
+    $display("y = %0d", y);
+
+    $finish;
+end
+
+endmodule
